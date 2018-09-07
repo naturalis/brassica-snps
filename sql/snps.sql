1ee0c0df37d2ccdc29375c65fed47ead459806e9
rvosa <rutgeraldo@gmail.com>
2018-09-07 13:04:30 +0000

    added SNPs table

--- sql/snps.sql
+++ sql/snps.sql
@@ -59,3 +59,33 @@ create table if not exists qtl_regions (
 	bsa_contrast text
 );
 .import qtl_regions.tsv qtl_regions
+
+create table if not exists snps ( 
+	snp_id integer constraint snp_pk primary key asc autoincrement, -- 1
+	chromosome_id integer constraint chromosome_fk references chromosomes (chromosome_id), -- 3
+	position integer, -- 13496034
+	ref text, -- A
+	alt text, -- T
+	ad_ref_low integer, -- 35
+	ad_alt_low integer, -- 37
+	dp_low integer, -- 72
+	pl_low text, -- "1253,0,1225"
+	gq_low integer, -- 99
+	snp_index_low real, -- 0.513888888888889
+	ad_ref_high integer, -- 36
+	ad_alt_high integer, -- 33
+	dp_high integer, -- 69
+	pl_high text, -- "1208,0,1404"
+	gq_high integer, -- 99
+	snp_index_high real, -- 0.478260869565217
+	ref_freq real, -- 0.50354609929078
+	delta_snp real, -- -0.0356280193236714
+	n_snps integer, -- 10626
+	tricube_delta_snp real, -- 0.0412010241428342
+	g real, -- 0.178946352691828
+	g_prime real, -- 2.50042210787922
+	p_value real, -- 0.0903783142874682
+	neg_log10_p_value real, -- 1.04393576327565
+	q_value real -- 0.55772879456407
+);
+.import snps.tsv snps
