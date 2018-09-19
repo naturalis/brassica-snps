afa7904ad2ee0b6d59edbae01bddecaf1fb70d10
Rutger Vos <rutgeraldo@gmail.com>
2018-09-19 14:03:28 +0200

    updated, annotated, as per #1

--- script/QTLseqr.R
+++ script/QTLseqr.R
@@ -1,14 +1,15 @@
 #!/usr/bin/Rscript
 
 # load the packages
-library("QTLseqr")
-library("dplyr")
+library(QTLseqr)
+library(dplyr)
+library(ggplot2)
 
 # possible bulks:
-# - group-1-EF
-# - group-2-IF
-# - group-3-LF
-# - group-5-NF
+# - group-1-EF - 11 accessions
+# - group-2-IF - 8 accessions
+# - group-3-LF - 11 accessions
+# - group-5-NF - 13 accessions
 
 # Set sample and file names
 HighBulk <- "group-2-IF"
@@ -26,14 +27,37 @@ df <- importFromGATK(
 	chromList = Chroms
 )
 
+# Optional: plot the read depth distribution
+# XXX the coverage *range* looks very similar to that in the vignette, although they have more
+# SNPs with low coverage, i.e. more skew towards the origin. Our data is actually a bit better.
+# ggplot(data = df) + 
+#  geom_histogram(aes(x = DP.HIGH + DP.LOW)) +
+#  xlim(0,1000)
+
+# Optional: plot total reference allele frequency
+# XXX this will look vastly different from the QTLseqr vignette because this is an
+# F1, so each reference allele will be present at least 50% (from the one, homozygous, T1000 parent) +
+# the other parent (Jersey kale), which might also have the allele. The average would be closer to 75%.
+# This would be of interest to come up with a value for refAlleleFreq, however, the default of 0.2 looks
+# reasonable. In fact, somewhat conservative due to the skewness in our distribution.
+# ggplot(data = df) +
+#  geom_histogram(aes(x = REF_FRQ))
+
+# Optional: plot per-bulk SNP-index
+# XXX as per the vignette, we do indeed see "two small peaks on each end", but the rest of the distribution
+# is not approximately normal because this is an F1
+# ggplot(data = df) +
+#  geom_histogram(aes(x = SNPindex.LOW))
+
 # Filter SNPs based on some criteria
 df_filt <- filterSNPs(
         SNPset = df,
-        refAlleleFreq = 0.20,
-        minTotalDepth = 100,
-        maxTotalDepth = 400,
-        minSampleDepth = 40,
-        minGQ = 99
+        refAlleleFreq = 0.20, # default, is conservative because F1 (see above)
+        minTotalDepth = 100, # default, at least 100x coverage, i.e. well supported SNPs
+        maxTotalDepth = 400, # default, no more than 400x coverage, i.e. avoid stacked repeats
+        minSampleDepth = 40, # default, per sample
+        minGQ = 99, # GATK's Genotype Quality score
+        verbose = TRUE
 )
 
 # Run G' analysis
@@ -45,7 +69,7 @@ df_filt <- runGprimeAnalysis(
 
 # write high G' snps to file
 snps <- filter(df_filt, Gprime > 2.5)
-write.csv(snps, file="SNPs-gprime2.5-EF-LF.csv")
+write.csv(snps, file="SNPs-gprime2.5-EF-IF.csv")
 
 # Plot G' results
 plotQTLStats(SNPset = df_filt, var = "Gprime", plotThreshold = TRUE, q = 0.01)
@@ -56,18 +80,15 @@ df_filt <- runQTLseqAnalysis(
         SNPset = df_filt,
         windowSize = 1e6,
         popStruc = "RIL",
-        bulkSize = c(11, 11),
+        bulkSize = c(10), # XXX see top of this file for bulk sizes. Average for RIL.
         replications = 10000,
         intervals = c(95, 99)
 )
-
-# Plot deltaSNP results
-plotQTLStats(SNPset = df_filt, var = "deltaSNP", plotIntervals = TRUE)
 
 # export summary CSV
 getQTLTable(
 	SNPset = df_filt, 
 	alpha = 0.01, 
 	export = TRUE, 
-	fileName = "SNPs-gprime-filtered-EF-IF.csv"
+	fileName = "QTL-regions-EF-IF.csv"
 )
