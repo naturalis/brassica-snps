270eab8c21fa7bf111442e01af06b263ce84dfb6
Rutger Vos <rutgeraldo@gmail.com>
2018-09-21 11:58:33 +0200

    Update make_fasta.sh

--- script/make_fasta.sh
+++ script/make_fasta.sh
@@ -4,7 +4,7 @@ DATA=/home/ubuntu/data
 REF=$DATA/reference/Brassica_oleracea.v2.1.dna.toplevel.chromosomes.fa
 BSA=$DATA/BSA
 BULKS="group-1-EF group-2-IF group-3-LF group-5-NF"
-SUFFIX=_pe.sorted.bam.RG.vcf.gz
+SUFFIX=_pe.sorted.bam.RG.vcf
 
 for BULK in $BULKS; do
   java -jar /usr/local/src/GenomeAnalysisTK.jar -T FastaAlternateReferenceMaker \
