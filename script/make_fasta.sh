625c7cc5e1ffafb923fe84fe49bd96db7f223291
Rutger Vos <rutgeraldo@gmail.com>
2018-09-21 17:10:47 +0200

    Now appends all CDSs to an outfile

--- script/make_fasta.sh
+++ script/make_fasta.sh
@@ -5,11 +5,21 @@ REF=$DATA/reference/Brassica_oleracea.v2.1.dna.toplevel.chromosomes.fa
 BSA=$DATA/BSA
 BULKS="group-1-EF group-2-IF group-3-LF group-5-NF"
 SUFFIX=_pe.sorted.bam.RG.vcf
+OUTFILE=merged.fasta
 
+# iterate over bulk assays
 for BULK in $BULKS; do
+
+  # create focal bulk's consensus sequence from SNPs and reference
   java -jar /usr/local/src/GenomeAnalysisTK.jar -T FastaAlternateReferenceMaker \
     -R ${REF} \
     -o ${BULK}.fasta \
     -L ${INTERVALS} \
     -V ${BSA}/${BULK}/${BULK}${SUFFIX}
+  
+  # append focal bulk's name as FASTA header
+  echo ">${BULK}" >> ${OUTFILE}
+  
+  # append all CDSs concatenated below the header
+  grep -v '>' ${BULK}.fasta >> ${OUTFILE}
 done
