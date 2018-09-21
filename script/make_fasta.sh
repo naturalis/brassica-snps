e49a64b71a25cda9c61f7dcdb61b195639b14359
Rutger Vos <rutgeraldo@gmail.com>
2018-09-21 11:22:43 +0200

    Create make_fasta.sh

---
+++ script/make_fasta.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+GENE=$1
+INTERVALS=$2
+DATA=/home/ubuntu/data
+REF=$DATA/reference/Brassica_oleracea.v2.1.dna.toplevel.chromosomes.fa
+BSA=$DATA/BSA
+BULKS="group-1-EF group-2-IF group-3-LF group-5-NF"
+SUFFIX=_pe.sorted.bam.RG.vcf.gz
+
+for BULK in $BULKS; do
+  gatk FastaAlternateReferenceMaker \
+    -R ${REF} \
+    -o ${BULK}.fasta \
+    -L ${INTERVALS} \
+    -V ${BSA}/${BULK}/${BULK}${SUFFIX}
+done
