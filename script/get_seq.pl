248e2c6e0f8c10d46556a0290a699ff8964dc7ad
Rutger Vos <rutgeraldo@gmail.com>
2018-09-25 13:36:22 +0200

    add VCF reader

---
+++ script/get_seq.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Vcf;
+use Getopt::Long;
+use Data::Dumper;
+use Log::Log4perl qw(:easy);
+Log::Log4perl->easy_init($DEBUG);
+
+# process command line arguments
+my $vcffile;
+my $intervals;
+GetOptions(
+  'vcffile=s'   => \$vcffile,
+  'intervals=s' => \$intervals,
+);
+
+# open intervals file, iterate over lines
+open my $fh, '<', $intervals or die $!;
+while(<$fh>) {
+  
+  # parse focal interval
+  chomp;
+  my $line = $_;
+  
+  # instantiate VCF reader
+  my $vcf = Vcf->new(
+    'file'   => $vcffile,
+    'region' => $line,
+  );
+  $vcf->parse_header();
+  
+  # do some simple parsing. Most thorough but slowest way how to get the data.
+  while( my $x = $vcf->next_data_hash() { 
+    DEBUG Dumper($x);
+  }
+}
