e829bcd0607438c5de127b6de35e83d2c166e79b
rvosa <rutgeraldo@gmail.com>
2018-11-08 14:57:46 +0000

    adding linkage to absolute locations script and result

--- script/blast_gene_map.pl
+++ script/blast_gene_map.pl
@@ -6,7 +6,7 @@ use Data::Dumper;
 use File::Temp qw(tempfile);
 use Bio::Tools::Run::StandAloneBlastPlus;
 use Log::Log4perl qw(:easy);
-Log::Log4perl->easy_init($ERROR);
+Log::Log4perl->easy_init($DEBUG);
 
 # process command line arguments
 my $infile    = '/home/ubuntu/data/reference/12864_2012_4560_MOESM1_ESM.tsv';
@@ -22,13 +22,15 @@ my $bp = Bio::Tools::Run::StandAloneBlastPlus->new( '-db_name' => $reference );
 # start reading the file
 my @header;
 open my $fh, '<', $infile or die $!;
-while(<$fh>) {
+LINE: while(<$fh>) {
   chomp;
   my @line = split /\t/, $_;
   
   # process header
   if ( not @header ) {
     @header = @line;
+    push @header, qw(fw_primer_start fw_primer_end rev_primer_start rev_primer_end);
+    print join("\t", @header), "\n";
   }
   
   # process record
@@ -36,17 +38,26 @@ while(<$fh>) {
     
     # populate the record
     my %record = map { $header[$_] => $line[$_] } 0 .. $#header;
+    next LINE if $record{'marker_type'} ne 'SNP';
     
+
     # run blast
-    my @results;
+    my $have_hits = 0;
     for ( qw(fw_primer rev_primer) ) {
       
       # do logging
       my $template = 'going to blast the %s for marker number %d (name: %s, type: %s) on chromosome %s';
-      DEBUG sprintf($template, @record{($_, qw(position marker_name marker_type linkage_group))});
-      push @results, do_blast(%record, 'primer_type' => $_);
+      DEBUG sprintf($template, $_, @record{qw(position marker_name marker_type linkage_group)});
+      my @results = do_blast(%record, 'primer_type' => $_);
+      if ( @results == 1 ) {
+        $record{$_ . '_start'} = $results[0]->{'start'};
+        $have_hits++;
+      }
     }
-    
+    if ( $have_hits ) {
+      no warnings 'uninitialized';
+      print join("\t", @record{@header}),"\n";
+    }
   }
 }
 
@@ -61,12 +72,23 @@ sub do_blast {
   # run query
   my $result = $bp->blastn( '-query' => $filename );
   my @hits;
-	while( my $hit = $result->next_hit ) {
-		if ( $hit->accession eq $args{'linkage_group'} ) {
-		  DEBUG "identity: " . $hit->frac_identical('total');
-		  print Dumper($hit);
-		}
-	}
-  
+  while( my $hit = $result->next_hit ) {
+
+    # only want hits on the same chromosome
+    if ( $hit->accession eq $args{'linkage_group'} ) {
+      my $ident = $hit->frac_identical('total');
+      DEBUG "identity: $ident";
+
+      # only want exact matches
+      if ( $ident == 1.0 ) {
+        my ( $start, $end ) = $hit->range('hit');
+        push @hits, {
+          'start' => $start,
+          'end'   => $end,
+        }
+      }
+    }
+  }
   unlink $filename;
+  return @hits;
 }
