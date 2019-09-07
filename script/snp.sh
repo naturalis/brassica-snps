733c0ee00c5471a8772e4ff2f128e22dfab3aa82
Rutger Vos <rutgeraldo@gmail.com>
2019-09-07 20:49:04 +0200

    updated to use DATA folder and to gzip the VCF file

--- script/snp.sh
+++ script/snp.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
 # reference genome
-REF=./reference/Brassica_oleracea.v2.1.dna.toplevel.chromosomes.fa
+REF=$DATA/reference/Brassica_oleracea.v2.1.dna.toplevel.chromosomes.fa
 
-# mapped genomes as sorted BAM files
-BAMS="./BSA/group-1-EF/group-1-EF_pe.sorted.bam ./BSA/group-2-IF/group-2-IF_pe.sorted.bam ./BSA/group-3-LF/group-3-LF_pe.sorted.bam ./BSA/group-5-NF/group-5-NF_pe.sorted.bam ./gDNA/group-4/group-4_pe.sorted.bam"
-
+# mapped bulks and genome as sorted BAM files
+BAMS="$DATA/BSA/group-1-EF/group-1-EF_pe.sorted.bam\
+ $DATA/BSA/group-2-IF/group-2-IF_pe.sorted.bam\
+ $DATA/BSA/group-3-LF/group-3-LF_pe.sorted.bam\
+ $DATA/BSA/group-5-NF/group-5-NF_pe.sorted.bam\
+ $DATA/gDNA/group-4/group-4_pe.sorted.bam"
 
 # iterate over BAM files
 for BAM in $BAMS; do
 
+  # which one are we doing now:
 	echo "operating on " ${BAM}
 
-	# index the BAM file
+	# index the BAM file if needed
 	if [ ! -f ${BAM}.bai ]; then
 		samtools index ${BAM}
 	fi
 
-	# do the haplotype calling
-	if [ ! -f ${BAM}.RG.vcf ]; then
+	# do the haplotype and compression if needed
+	if [ ! -f ${BAM}.RG.vcf.gz ]; then
 		gatk HaplotypeCaller \
 			--reference ${REF} \
 			--input ${BAM} \
 			--output ${BAM}.RG.vcf \
 			--emit-ref-confidence GVCF
+		gzip ${BAM}.RG.vcf
 	fi
 done
 
