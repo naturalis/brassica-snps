7cce80e50eea2c573b2b4f5aa30a7d11c3c881ca
Rutger Vos <rutgeraldo@gmail.com>
2021-08-16 15:29:56 +0200

    # documented in supplementary materials

--- sql/make_dbix_api.sh
+++ sql/make_dbix_api.sh
@@ -1,2 +1,3 @@
+# documented in supplementary materials
 perl -MDBIx::Class::Schema::Loader=make_schema_at,dump_to_dir:./lib -e 'make_schema_at("My::Brassica", { debug => 1 }, [ "dbi:SQLite:dbname=snps.db","sqlite" ])'
 
